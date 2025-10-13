fa4953619849a15d032a21ba173a1c92c3ce98aa
HB <hunter034@revature.net>
2025-10-13 15:00:33 -0400

    Created repositories

---
+++ game-store/src/main/java/com/example/game_store/repo/GameRepository.java
@@ -0,0 +1,18 @@
+package com.example.game_store.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.game_store.model.Game;
+import java.util.List;
+
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+    Game findById(long id);
+    @Query("SELECT g FROM Game g WHERE LOWER(g.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    List<Game> findByTitle(@Param("title") String title);
+
+}
---
+++ game-store/src/main/java/com/example/game_store/repo/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.game_store.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.game_store.model.User;
+
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(long id);
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :username, '%'))")
+    List<User> findByUsername(@Param("username") String username);
+
+}
