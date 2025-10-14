140f242ec6513ad41a3143f586fec9590e2c684f
Suman Subedi <ssubedi60@gmail.com>
2025-10-14 15:16:38 -0500

    Renamed the Repo Folder to Repository for better readability

--- game-store/src/main/java/com/example/game_store/repository/GameRepository.java
+++ game-store/src/main/java/com/example/game_store/repository/GameRepository.java
@@ -1,4 +1,4 @@
-package com.example.game_store.repo;
+package com.example.game_store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Repository;
 import com.example.game_store.model.Game;
 import java.util.List;
 
-
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     Game findById(long id);
+
     @Query("SELECT g FROM Game g WHERE LOWER(g.title) LIKE LOWER(CONCAT('%', :title, '%'))")
     List<Game> findByTitle(@Param("title") String title);
 
